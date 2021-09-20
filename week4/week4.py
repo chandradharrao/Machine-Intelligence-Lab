ef47285f244df89465a5424684bfd27d947d0639
chandradharrao <chandradhar.rao@gmail.com>
2021-09-20 18:06:50 +0530

    passed all test cases but not optimized

--- week4/week4.py
+++ week4/week4.py
@@ -1,8 +1,6 @@
 import numpy as np
 import math
 
-from pandas.core.dtypes.missing import isna
-
 class KNN:
     """
     K Nearest Neighbours model
@@ -34,7 +32,7 @@ class KNN:
         """
 
         self.data = data #store the trainig data
-        self.target = target.astype(np.int64) #stores the target for each of the data point
+        self.target = target.astype(np.int64) #stores the target for each of the data point as discrete values only
 
         return self
     
@@ -76,20 +74,6 @@ class KNN:
         for c in x: #for each of the query_point in list of query_points
             temp=[]
             for instance in self.data: #calc dist b/w data points
-
-                #ignore the query point if it is present in the dataset
-                #print(f"Query point with D dims {c}")
-                #print(f"The instace is {instance}")
-                #print(f"Types {type(c)} vs {type(instance)}")
-                n=len(c)
-                m=len(instance)
-                if n==m:
-                    same=True
-                    for i in range(n):
-                        if c[i]!=instance[i]:
-                            same=False
-                    if same: continue
-
                 temp.append(self.minkowski_dist(c,instance))
             dists.append(temp)
         # print("Distances of every query point with dataset is ")
@@ -97,12 +81,6 @@ class KNN:
         # for d in dists:
         #     print(d)
 
-        return dists
-
-    def euclids_dist(self,vec1,vec2):
-        dists=0
-        for i in range(len(vec1)):
-            dists+=math.sqrt(math.pow(vec1[i],2)+math.pow(vec2[i],2))
         return dists
 
     """
@@ -134,7 +112,6 @@ class KNN:
             #sort the distances along with the index
             indxs=np.argsort(c_dist)
 
-            #0th indx will be distance of c to itself
             knn_indxs.append(indxs[0:self.k_neigh])
             knn_dists.append([c_dist[i] for i in indxs][0:self.k_neigh])
 
@@ -159,7 +136,7 @@ class KNN:
 
         '''
         Edge cases:
-        Try finding mean using numpy 
+        Try finding mode with numpy
         '''
         
         k_nei_dists,k_nei_indxs = self.k_neighbours(x) #n knn_indx and knn_dists 
@@ -169,31 +146,51 @@ class KNN:
             if self.weighted:
                 cumu_wts={}
                 for a_dist,a_indx in zip(k_nei_dist,k_nei_indx):
+                    k=f"{self.target[a_indx]}"
+                    if k not in cumu_wts:
+                        cumu_wts[k]=0
+
+                    if a_dist==0:
+                        cumu_wts[k]=float("inf")
+                        continue
+
                     wt=1/a_dist
-                    cumu_wts[f"{self.target[a_indx]}"]+=wt
+                    cumu_wts[k]+=wt
                 
                 #max weight ie inv of distance
                 max_wt_node=float("-inf")
                 max_wt=float("-inf")
-                for k,v in cumu_wts:
+                #print(f"Cumu wts {cumu_wts}")
+                for k,v in cumu_wts.items():
                     k=int(k)
                     if v>max_wt or v==max_wt and k<max_wt_node:
                         max_wt=v
                         max_wt_node=k
                 pred.append(max_wt_node)
-                
             else:
-                #go through the k neighbours and calc the target mean since only continous variables
-                mean=0
-                n=0
-                for i in k_nei_indx:
-                    mean+=self.target[i]
-                    n+=1
-                mean=mean/n
-                pred.append(int(mean))
+                #go through the k neighbours and which class they belong to 
+                #return the class with highest mode
+                #in case of conflict lexi
+                max_label=float("-inf")      
+                max_mode=float("-inf")
+                mode={}
 
-                print(f"Mean {mean}")
-        print(f"Pred {pred}")
+                for i in k_nei_indx:
+                    k=f"{self.target[i]}"
+                    if k not in mode:
+                        mode[k]=1
+                    else:
+                        mode[k]+=1
+                #print(f"Mode {mode}")
+                for k,v in mode.items():
+                    k=int(k)
+                    if v>max_mode or v==max_mode and k<max_label:
+                        max_label=k
+                        max_mode=v
+                pred.append(max_label)
+                #print(f"Mode {mode}")
+
+        #print(f"Pred {pred}")
         return pred
                 
 
@@ -213,46 +210,49 @@ class KNN:
         for i in range(n):
             if preds[i]==y[i]:
                 correct+=1
-        return correct/n
+        #print(f"Acc {(correct/n)*100}")
+        return (correct/n)*100
 
 
-if __name__=="__main__":
-    data1=[
-        [5,45],
-        [5.11,26],
-        [5.6,30],
-        [5.9,34],
-        [4.8,40],
-        [5.8,36],
-        [5.3,19],
-        [5.8,28],
-        [5.5,23],
-        [5.6,32],
-    ]
-    data1=np.asarray(data1)
 
-    target1=[
-        77,
-        47,
-        55,
-        59,
-        72,
-        60,
-        40,
-        60,
-        45,
-        58
-    ]
-    target1=np.asarray(target1)
-    query1=[
-        [5.5,38]
-    ]
-    query_target1=[
-        63.666
-    ]
-    query_target1=np.asarray(query_target1)
-    query_target1=query_target1.astype(np.int64)
-    query1=np.asarray(query1)
+# if __name__=="__main__":
+    #test_case2()
+    # data1=[
+    #     [5,45],
+    #     [5.11,26],
+    #     [5.6,30],
+    #     [5.9,34],
+    #     [4.8,40],
+    #     [5.8,36],
+    #     [5.3,19],
+    #     [5.8,28],
+    #     [5.5,23],
+    #     [5.6,32],
+    # ]
+    # data1=np.asarray(data1)
+
+    # target1=[
+    #     77,
+    #     47,
+    #     55,
+    #     59,
+    #     72,
+    #     60,
+    #     40,
+    #     60,
+    #     45,
+    #     58
+    # ]
+    # target1=np.asarray(target1)
+    # query1=[
+    #     [5.5,38]
+    # ]
+    # query_target1=[
+    #     63.666
+    # ]
+    # query_target1=np.asarray(query_target1)
+    # query_target1=query_target1.astype(np.int64)
+    # query1=np.asarray(query1)
 
     # data=[
     #     [100,120,12,6],
@@ -279,11 +279,56 @@ if __name__=="__main__":
     # ]
     # query_points=np.asarray(query_points)
     # query_targets=np.array(query_targets)
-    
 
-    knn = KNN(3,False,2)
-    knn.fit(data1,target1)
-    acc= knn.evaluate(query1,query_target1)
-    print(f"The accuracy is {acc*100}%")
+    # knn = KNN(3,False,2)
+    # knn.fit(data1,target1)
+    # acc= knn.evaluate(query1,query_target1)
+    # print(f"The accuracy is {acc}%")
 
+
+# def test_case2():
+#     data = np.array([[0.68043616, 0.39113473, 0.1165562 , 0.70722573, 0],
+#        [0.67329238, 0.69782966, 0.73278321, 0.78787406, 0],
+#        [0.56134898, 0.25358895, 0.10497708, 0.05846073, 1],
+#        [0.6515744 , 0.85627836, 0.44305142, 0.53280211, 0],
+#        [0.47014548, 0.18108572, 0.3235044 , 0.45490616, 0],
+#        [0.33544621, 0.51322212, 0.98769111, 0.53091437, 0],
+#        [0.4577167 , 0.80579291, 0.19350921, 0.46502849, 0],
+#        [0.25709202, 0.06937377, 0.92718944, 0.54662592, 1],
+#        [0.07637632, 0.3176806 , 0.74102328, 0.32849423, 1],
+#        [0.2334587 , 0.67725537, 0.4323325 , 0.38766629, 0]])
+
+#     X_train = data[:, :4]
+#     y_train = data[:, 4]
+#     samples = np.array([[0.41361609, 0.45603303, 0.33195254, 0.09371524, 1],
+#        [0.19091752, 0.07588166, 0.03198771, 0.15245555, 1],
+#        [0.29624916, 0.80906772, 0.35025253, 0.78940926, 0],
+#        [0.96729604, 0.89730852, 0.39105022, 0.37876973, 0],
+#        [0.52963052, 0.29303055, 0.27697515, 0.67815307, 1]])
+#     X_test = samples[:, :4]
+#     y_test = samples[:, 4]
+#     kneigh_dist = np.array([[0, 0.87960746, 0.91697707], [0, 0.72497042, 1.01071404]])
+#     kneigh_idx = np.array([[0, 4, 2], [1, 3, 0]])
+#     pred = np.array([0, 1, 0, 0, 0])
+
+#     model = KNN(k_neigh = 3, p = 1, weighted=True)
+#     model.fit(X_train, y_train)
+#     try:
+#         np.testing.assert_array_almost_equal(
+#             model.k_neighbours(X_train[0:2, :])[0], kneigh_dist, decimal=2)
+#         print("Test Case 1 for the function k_neighbours (distance) PASSED")
+#     except:
+#         print("Test Case 1 for the function k_neighbours (distance) FAILED")
+
+#     try:
+#         np.testing.assert_array_equal(
+#             model.k_neighbours(X_train[0:2, :])[1], kneigh_idx)
+#         print("Test Case 2 for the function k_neighbours (idx) PASSED")
+#     except:
+#         print("Test Case 2 for the function k_neighbours (idx) FAILED")
+
+#     np.testing.assert_array_equal(
+#             model.predict(X_test), pred)
+
+#     assert model.evaluate(X_test, y_test) == 60
 
